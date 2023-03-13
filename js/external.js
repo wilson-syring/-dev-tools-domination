c7ae182f4f0df757a4eec40c9fce7cc73a2d5956
Wilson Syring <wilson.syring@gmail.com>
2023-03-13 11:28:38 -0500

    mostly finished with console log exercise didnt realize there were so many ways to use this

--- js/external.js
+++ js/external.js
@@ -7,16 +7,22 @@ function makeGreen() {
 }
 
 // Regular
+console.log('welcome');
 
 // Interpolated
+console.log('welcome to the %s show','💩');
 
 // Styled
+console.log('%c Welcome to the thunderdome!','font-size: 25px; background:red; text-shadow: 5px 5px 0 black; color: green; ');
 
 // warning!
+console.warn('Warning!');
 
 // Error :|
+console.error('❌');
 
 // Info
+console.info();
 
 // Testing
 
